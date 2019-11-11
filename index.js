5314b88abfe83311919cd70cb8d1f94c9db7f949
Tania Manco <belalov.tania16@gmail.com>
2019-11-11 20:10:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  
+  driver[key] = value;
+  return driver;
 }
